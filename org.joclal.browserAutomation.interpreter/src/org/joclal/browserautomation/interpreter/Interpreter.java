7d28db4eac9ad9ce094fc93779536f89df67c14c
cguyonnet <clement.guyonnet@gmail.com>
2015-02-13 14:14:06 +0100

    Added Fill, Click and Goto op in interpreter

--- org.joclal.browserAutomation.interpreter/src/org/joclal/browserautomation/interpreter/Interpreter.java
+++ org.joclal.browserAutomation.interpreter/src/org/joclal/browserautomation/interpreter/Interpreter.java
@@ -1,8 +1,12 @@
 package org.joclal.browserautomation.interpreter;
 
+import java.util.List;
+
 import org.joclal.browserAutomation.*;
 import org.joclal.browserautomation.interpreter.utils.InterpreterUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -48,14 +52,92 @@ public class Interpreter {
 		}
 
 	}
-	
-	private void processAction(Action a){
-		//a.
-	}
 
 	private void init() {
 		// browser props
 		System.setProperty("webdriver.chrome.driver", "res/chromedriver");
 	}
+	
+	private void processAction(Action a){
+		if(a instanceof Goto){
+			processGoto((Goto) a);
+		}else if(a instanceof ClickOn){
+			processClickOn((ClickOn) a);
+		}else if(a instanceof Fill){
+			processFill((Fill) a);
+		}else if(a instanceof Check){
+			processCheck((Check) a);
+		}else if(a instanceof Uncheck){
+			processUncheck((Uncheck) a);
+		}else if(a instanceof Let){
+			processLet((Let) a);
+		}else if(a instanceof DoWhile){
+			processDoWhile((DoWhile) a);
+		}else if(a instanceof IfThen){
+			processIfThen((IfThen) a);
+		}else if(a instanceof SubroutineCall){
+			processSubroutineCall((SubroutineCall) a);
+		}
+	}
+	
+	private void processSubroutineCall(SubroutineCall a) {
+		// TODO Auto-generated method stub
+		
+	}
 
+	private void processIfThen(IfThen a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void processDoWhile(DoWhile a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void processLet(Let a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void processUncheck(Uncheck a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void processCheck(Check a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void processFill(Fill a) {
+		Selector s = a.getField();
+		List<String> sels = s.getHaydies();
+		for(String sel : sels){
+			WebElement element = this.getWebElement(sel);
+			element.sendKeys(InterpreterUtils.getValue(a.getValue()));
+		}
+		
+	}
+
+	private void processClickOn(ClickOn a) {
+		Selector s = a.getSelector();
+		List<String> sels = s.getHaydies();
+		for(String sel : sels){
+			WebElement element = this.getWebElement(sel);
+			element.click();
+		}
+	}
+
+	private void processGoto(Goto a){
+		Value urlValue = a.getUrl();
+		String url = InterpreterUtils.getValue(urlValue);
+		driver.get(url);
+	}
+
+	
+	private WebElement getWebElement(String sel) {
+		return driver.findElement(By.name(sel));
+	}
+	
 }
